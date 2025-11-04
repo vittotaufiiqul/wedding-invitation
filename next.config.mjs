35941ea5eca5591ba03f404fc3ab2d2521f73c12
Vitto Hakim <taufiiqulhakim17@gmail.com>
2025-11-04 17:49:18 +0700

    fix: remove reactCompiler and replace Geist font

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
-
-export default nextConfig;
+const nextConfig = {}
+export default nextConfig
